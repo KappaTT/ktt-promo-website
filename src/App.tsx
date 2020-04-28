1a5311fe1652d254769cf0a82bb6b2772e6746a6
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-28 15:19:54 -0400

    Initial build

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="screenshot-container">
+        <div className="screenshots">
+          <div className="screenshot screenshot-1"></div>
+          <div className="screenshot screenshot-2"></div>
+          <div className="screenshot screenshot-3"></div>
+        </div>
+      </div>
+      <div className="frame-container">
+        <div className="frame">
+          <img src="iphone-frame.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
