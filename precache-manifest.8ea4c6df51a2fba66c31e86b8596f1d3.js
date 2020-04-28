abdea3a1f1631b241a9ea520ebbb9f31686f165d
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-28 15:24:54 -0400

    Updates

---
+++ precache-manifest.8ea4c6df51a2fba66c31e86b8596f1d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbee46f62adfc0a591787397f44de21",
+    "url": "/ktt-promo-website/index.html"
+  },
+  {
+    "revision": "bf8a4e8fa18145fa9631",
+    "url": "/ktt-promo-website/static/css/main.ef57e7c5.chunk.css"
+  },
+  {
+    "revision": "e7110de833a1a8308266",
+    "url": "/ktt-promo-website/static/js/2.fffc5821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ktt-promo-website/static/js/2.fffc5821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8a4e8fa18145fa9631",
+    "url": "/ktt-promo-website/static/js/main.fec5484c.chunk.js"
+  },
+  {
+    "revision": "0bddd39cbe8261ef4d92",
+    "url": "/ktt-promo-website/static/js/runtime-main.de9b0c9d.js"
+  },
+  {
+    "revision": "7da619b530c1c8de1b2c0830a8ae21e1",
+    "url": "/ktt-promo-website/static/media/screenshot-1.7da619b5.png"
+  },
+  {
+    "revision": "c65a76a390319662dbc50ba3ff1c9b71",
+    "url": "/ktt-promo-website/static/media/screenshot-2.c65a76a3.png"
+  },
+  {
+    "revision": "44837545d5be720ad277b6753943e831",
+    "url": "/ktt-promo-website/static/media/screenshot-3.44837545.png"
+  }
+]);
